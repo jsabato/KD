a63771d0b25472634b165f2c4574d59749daa538
Joseph Sabato <Reticulin@Josephs-MacBook-Air.local>
2016-01-08 09:37:45 -0800

    First commit

---
+++ db/migrate/20160108172016_create_items.rb
@@ -0,0 +1,12 @@
+class CreateItems < ActiveRecord::Migration
+  def change
+    create_table :items do |t|
+      t.references :client
+      t.string :item_name
+      t.string :item_count
+      t.string :item_description
+      t.string :item_price
+      t.timestamps
+    end
+  end
+end
